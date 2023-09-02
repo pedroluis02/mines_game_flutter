97917e16f9ad01b276e89eb035182f2c6fe23066
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-01 23:52:05 -0500

    setuo log and full screen mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:developer' as dev;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:logging/logging.dart';
+
+Logger _log = Logger('main.dart');
 
 void main() {
+  _setup();
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
+}
+
+void _setup() {
+  _setupLogger();
+  _setupFullScreenMode();
+}
+
+void _setupLogger() {
+  Logger.root.onRecord.listen((record) {
+    dev.log(
+      record.message,
+      time: record.time,
+      level: record.level.value,
+      name: record.loggerName,
+      zone: record.zone,
+      error: record.error,
+      stackTrace: record.stackTrace,
+    );
+  });
+}
+
+void _setupFullScreenMode() {
+  _log.info('Going full screen');
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+  );
 }
 
 class MyApp extends StatelessWidget {
