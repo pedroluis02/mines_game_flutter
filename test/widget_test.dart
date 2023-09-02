f4da439940442ceba7cdffffc701b507dfe8fe7b
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-01 23:56:20 -0500

    refactor(test): import MyApp from app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mines_game_flutter/main.dart';
+import 'package:mines_game_flutter/app.dart';
 
 void main() {
   testWidgets('Some test', (WidgetTester tester) async {
