d83576b365368901bb65abe9f73017a3a1e65734
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-05 15:26:41 -0500

    feat(ui): MainScree uses StatefulHookConsumerWidget

--- lib/src/main_screen/main_screen.dart
+++ lib/src/main_screen/main_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MainScreen extends StatefulHookWidget {
+class MainScreen extends StatefulHookConsumerWidget {
   const MainScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  ConsumerState<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends ConsumerState<MainScreen> {
   @override
   Widget build(BuildContext context) {
     final appLifeCycleState = useAppLifecycleState();
