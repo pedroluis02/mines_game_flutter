3eb7ecc4beaf2033e963e49725ca46a7c47c803c
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-05 15:29:49 -0500

    feat(ui): MainScreen usess HookConsumerWidget

--- lib/src/main_screen/main_screen.dart
+++ lib/src/main_screen/main_screen.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MainScreen extends StatefulHookConsumerWidget {
+class MainScreen extends HookConsumerWidget {
   const MainScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  ConsumerState<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends ConsumerState<MainScreen> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final appLifeCycleState = useAppLifecycleState();
     debugPrint('appLifeCycleState: ${appLifeCycleState?.name}');
 
@@ -23,8 +18,11 @@ class _MainScreenState extends ConsumerState<MainScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text(title),
       ),
       body: const Center(
         child: Text('Mines Game - Flutter casual game toolkit'),
