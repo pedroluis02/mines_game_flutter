55dc8514db7cbde37058b0a8d57cc42312cf4651
Pedro Luis <pedroluisnr22@gmail.com>
2023-10-04 21:00:01 -0500

    feat(ui): add responsive screen from template

---
+++ lib/src/style/responsive_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveScreen extends StatelessWidget {
+  final Widget squarishMainArea;
+  final Widget rectangularMenuArea;
+  final Widget topMessageArea;
+  final double mainAreaProminence;
+
+  const ResponsiveScreen({
+    required this.squarishMainArea,
+    required this.rectangularMenuArea,
+    this.topMessageArea = const SizedBox.shrink(),
+    this.mainAreaProminence = 0.8,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final size = constraints.biggest;
+        final padding = EdgeInsets.all(size.shortestSide / 30);
+
+        if (size.height >= size.width) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SafeArea(
+                bottom: false,
+                child: Padding(
+                  padding: padding,
+                  child: topMessageArea,
+                ),
+              ),
+              Expanded(
+                flex: (mainAreaProminence * 100).round(),
+                child: SafeArea(
+                  top: false,
+                  bottom: false,
+                  minimum: padding,
+                  child: squarishMainArea,
+                ),
+              ),
+              SafeArea(
+                top: false,
+                maintainBottomViewPadding: true,
+                child: Padding(
+                  padding: padding,
+                  child: rectangularMenuArea,
+                ),
+              ),
+            ],
+          );
+        } else {
+          final isLarge = size.width > 900;
+
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: isLarge ? 7 : 5,
+                child: SafeArea(
+                  right: false,
+                  maintainBottomViewPadding: true,
+                  minimum: padding,
+                  child: squarishMainArea,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    SafeArea(
+                      bottom: false,
+                      left: false,
+                      maintainBottomViewPadding: true,
+                      child: Padding(
+                        padding: padding,
+                        child: topMessageArea,
+                      ),
+                    ),
+                    Expanded(
+                      child: SafeArea(
+                        top: false,
+                        left: false,
+                        maintainBottomViewPadding: true,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                            padding: padding,
+                            child: rectangularMenuArea,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        }
+      },
+    );
+  }
+}
