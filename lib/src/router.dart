ae241c61ac3d14cda9e007f4d51c73eac546ba1b
Pedro Luis <pedroluisnr22@gmail.com>
2023-10-04 22:06:26 -0500

    feat(ui): register /play route

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -1,18 +1,35 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mines_game_flutter/src/style/my_transition.dart';
 
 import 'main_screen/main_screen.dart';
+import 'play_session/play_session_screen.dart';
 
-GoRouter buildRouter() {
-  return GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (_, __) => const MainScreen(
-          title: 'Mines',
-          key: Key('MainScreen'),
+final class MainGoRouterCreator {
+  GoRouter create() {
+    return GoRouter(
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (_, __) => const MainScreen(
+                  title: 'Mines',
+                  key: Key('MainScreen'),
+                ),
+            routes: [_playRoute()])
+      ],
+    );
+  }
+
+  GoRoute _playRoute() {
+    return GoRoute(
+      path: 'play',
+      pageBuilder: (context, __) => buildMyTransition<void>(
+        key: const ValueKey('play'),
+        color: Theme.of(context).primaryColor,
+        child: const PlaySessionScreen(
+          key: Key('PlayGame'),
         ),
-      )
-    ],
-  );
+      ),
+    );
+  }
 }
